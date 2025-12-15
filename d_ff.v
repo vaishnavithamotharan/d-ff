1f0631d6983e69e2382853434149236cf648482c
vaishnavithamotharan <vaishnavithamotharan3@gmail.com>
2025-12-15 23:53:05 +0530

    Add files via upload

---
+++ d_ff.v
@@ -0,0 +1,14 @@
+module d_ff (
+    input  wire clk, rst, D,
+    output reg  Q
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            Q <= 1'b0;   
+        else
+            Q <= D;      
+    end
+endmodule
+
+
+
